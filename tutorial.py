279882a274cd2cea951e4b5562280ae54a21fa8f
jbrighter98 <44352550+jbrighter98@users.noreply.github.com>
2018-12-04 23:12:31 -0500

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -4,6 +4,18 @@ from tkinter import ttk
  
 #tutorial = Tk()
 
+toolbar = "Images/toolbar.gif"
+chordbuilder = "Images/chordbuilder.gif"
+altbuild = "Images/altbuild.gif"
+scalebuild = "Images/scalebuild.gif"
+helpmenu = "Images/helpmenu.gif"
+optionsmenu = "Images/optionsmenu.gif"
+tuningmenu = "Images/tuningmenu.gif"
+cdotcolorblind = "Images/cdotcolorblind.gif"
+cdotdefault = "Images/cdotdefault.gif"
+cshapedefault = "Images/cshapedefault.gif"
+guitardisplay = "Images/guitardisplay.gif"
+
 def user_tutorial(root):
     tutorial = Toplevel(root)
 
@@ -32,17 +44,17 @@ def user_tutorial(root):
     nbook.add(page9, text=' Chord and Scale Display ')
     nbook.pack(expan = 1, fill = 'both')
 
-    toolbar = "Images/toolbar.gif"
-    chordbuilder = "Images/chordbuilder.gif"
-    altbuild = "Images/altbuild.gif"
-    scalebuild = "Images/scalebuild.gif"
-    helpmenu = "Images/helpmenu.gif"
-    optionsmenu = "Images/optionsmenu.gif"
-    tuningmenu = "Images/tuningmenu.gif"
-    cdotcolorblind = "Images/cdotcolorblind.gif"
-    cdotdefault = "Images/cdotdefault.gif"
-    cshapedefault = "Images/cshapedefault.gif"
-    guitardisplay = "Images/guitardisplay.gif"
+    """toolbar.label = "Images/toolbar.gif"
+    chordbuilder.label = "Images/chordbuilder.gif"
+    altbuild.label = "Images/altbuild.gif"
+    scalebuild.label = "Images/scalebuild.gif"
+    helpmenu.label = "Images/helpmenu.gif"
+    optionsmenu.label = "Images/optionsmenu.gif"
+    tuningmenu.label = "Images/tuningmenu.gif"
+    cdotcolorblind.label = "Images/cdotcolorblind.gif"
+    cdotdefault.label = "Images/cdotdefault.gif"
+    cshapedefault.label = "Images/cshapedefault.gif"
+    guitardisplay.label = "Images/guitardisplay.gif"""
 
     labelframe1 = ttk.LabelFrame(page7)
     labelframe1.grid(column=1,row=0,sticky='N')
@@ -50,39 +62,61 @@ def user_tutorial(root):
     labelframe2 = ttk.LabelFrame(page7)
     labelframe2.grid(column=2,row=0,sticky='N')
 
-     
+         
     img1 = PhotoImage(file=toolbar)
-    g1 = Label(page2, image=img1).pack(side = 'left')
+    g1 = ttk.Label(page2, image=img1)
+    g1.image = img1
+    g1.pack(side = 'left')
 
     img2 = PhotoImage(file=chordbuilder)
-    g2 = Label(page3, image=img2).pack(side = 'left')
+    g2 = ttk.Label(page3, image=img2)
+    g2.image = img2
+    g2.pack(side = 'left')
 
     img3 = PhotoImage(file=altbuild)
-    g3 = Label(page4, image=img3).pack(side = 'left')
+    g3 = ttk.Label(page4, image=img3)
+    g3.image = img3
+    g3.pack(side = 'left')
 
     img4 = PhotoImage(file=scalebuild)
-    g4 = Label(page5, image=img4).pack(side = 'left')
+    g4 = ttk.Label(page5, image=img4)
+    g4.image = img4
+    g4.pack(side = 'left')
 
     img5 = PhotoImage(file=tuningmenu)
-    g5 = Label(page6, image=img5).pack(side = 'left')
+    g5 = ttk.Label(page6, image=img5)
+    g5.image = img5
+    g5.pack(side = 'left')
 
     img6 = PhotoImage(file=optionsmenu)
-    g6 = Label(labelframe1, image=img6).grid(column = 1, row = 1)
+    g6 = ttk.Label(labelframe1, image=img6)
+    g6.image = img6
+    g6.grid(column = 1, row = 1)
 
     img7 = PhotoImage(file=cdotdefault)
-    g7 = Label(labelframe1, image=img7).grid(column = 1, row = 2)
+    g7 = ttk.Label(labelframe1, image=img7)
+    g7.image = img7
+    g7.grid(column = 1, row = 2)
 
     img8 = PhotoImage(file=cdotcolorblind)
-    g8 = Label(labelframe1, image=img8).grid(column = 1, row = 3)
+    g8 = ttk.Label(labelframe1, image=img8)
+    g8.image = img8
+    g8.grid(column = 1, row = 3)
 
     img9 = PhotoImage(file=cshapedefault)
-    g9 = Label(labelframe1, image=img9).grid(column = 1, row = 4)
+    g9 = ttk.Label(labelframe1, image=img9)
+    g9.image = img9
+    g9.grid(column = 1, row = 4)
 
     img10 = PhotoImage(file=helpmenu)
-    g10 = Label(page8, image=img10).pack(side = 'left')
+    g10 = ttk.Label(page8, image=img10)
+    g10.image = img10
+    g10.pack(side = 'left')
 
     img11 = PhotoImage(file=guitardisplay)
-    g11 = Label(page9, image=img11).pack()
+    g11 = ttk.Label(page9, image=img11)
+    g11.image = img11
+    g11.pack()
 
 
     with open('tutorialtext.txt','r') as help_file:
